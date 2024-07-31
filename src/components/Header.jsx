a80c18c496988c108ddaa5e9fe01be51d5dcf766
Adham M <adhammasoud2002@gmail.com>
2024-07-31 17:54:37 +0300

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,10 @@ function Header(){
     return(
         
         <nav className="header">
-            <h1 className="title"><a  href="/home">LuxeWatch</a></h1>
+            <h1 className="title"><a  href="/LuxeWatch/home">LuxeWatch</a></h1>
             
             <div className="rightplace">
-                <a className="btn" href="/about">About</a>
+                <a className="btn" href="/LuxeWatch/about">About</a>
                 <a className="btn" href="/products">Products</a>
                 <a className="btn" href="/contact">Contact</a>
                 <a className="btn" href="/account">Account</a>
